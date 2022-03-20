3926e0d44e728e0b6715c1ec38787868c98be9ff
Aale Raza <aale.raza.saufik@outlook.com>
2022-03-20 14:26:41 +0500

    Change in package for select list item

--- Billing.DTOs/DTOs/OrganizationDTO.cs
+++ Billing.DTOs/DTOs/OrganizationDTO.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
 using static Billing.Enum.OrganizationEnum;
 
 namespace Billing.DTOs.DTOs
