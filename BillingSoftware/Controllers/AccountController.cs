faa5837edf31e479a3cdd1feac114ecc59d258fc
Aale Raza <aale.raza.saufik@outlook.com>
2022-03-20 14:19:37 +0500

    Add login page and their functionality

---
+++ BillingSoftware/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Billing.Data.Entities;
+using Billing.DTOs.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BillingSoftware.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<Users> _signInManager;
+        private readonly UserManager<Users> _userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginDTO());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO Input)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, true, true);
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.FindByEmailAsync(Input.Email);
+                    var userRole = await _userManager.GetRolesAsync(user);
+                    if (!user.EmailConfirmed)
+                    {
+                        await _signInManager.SignOutAsync();
+                        ModelState.AddModelError(string.Empty, "You Email is not verified please verify your Email Thanks.");
+                        return View();
+                    }
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
--- BillingSoftware/Startup.cs
+++ BillingSoftware/Startup.cs
@@ -58,7 +58,7 @@ namespace BillingSoftware
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
