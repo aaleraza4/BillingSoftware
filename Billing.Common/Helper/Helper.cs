0a7356cc2c5bf93f7e9d0b1db5813a7e96fbf645
Aale Raza <aale.raza.saufik@outlook.com>
2022-03-20 14:28:42 +0500

    minor change in helper class

--- Billing.Common/Helper/Helper.cs
+++ Billing.Common/Helper/Helper.cs
@@ -1,10 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
 
 namespace Billing.Common.Helper
 {
